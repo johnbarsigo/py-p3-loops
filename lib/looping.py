91d147a68f5ed7059ddf84e8815ba7b248331103
john.barsigo@student.moringaschool.com <john.barsigo@student.moringaschool.com>
2024-03-04 09:43:56 +0300

    Completed assignment

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
+    counter = 10
+    while counter > 0 :
+        print (counter)
+        counter -= 1
+    print ("Happy New Year!")
     pass
 
 def square_integers(int_list):
     # code goes here!
-    pass
+    int_list = [ int * int for int in int_list ]
+    return int_list
 
 def fizzbuzz():
     # code goes here!
+    
+    for counter in range (1, 101) :
+        if counter % 3 == 0 and counter % 5 == 0 :
+            print ("FizzBuzz")
+        elif counter % 3 == 0 :
+            print ("Fizz")
+        elif counter % 5 == 0 :
+            print ("Buzz")
+        else :
+            print (counter)
     pass
